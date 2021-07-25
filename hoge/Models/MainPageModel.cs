548f6bef82489533e54beb579cb5dd5c7c36fea6
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2021-07-25 19:13:41 +0900

    feat: service sample

--- hoge/Models/MainPageModel.cs
+++ hoge/Models/MainPageModel.cs
@@ -1,21 +1,39 @@
 ﻿using Prism.Mvvm;
+using hoge.Services;
 using System.Threading.Tasks;
 
 namespace hoge.Models
 {
     public class MainPageModel : BindableBase
     {
+        private ApiGetService ApiGetService = new ApiGetService();
+
         public MainPageModel()
         {
             this.count = 0;
         }
 
         private int count;
+        // TODO: 型定義がAPIの返り値の形になるように修正する。
+        private int probrems;
+        private bool loading;
 
         public int Count
         {
             get { return count; }
             set { SetProperty(ref this.count, value); }
+        }
+
+        public int Probrems
+        {
+            get { return probrems; }
+            set { SetProperty(ref probrems, value); }
+        }
+
+        public bool Loading
+        {
+            get { return loading; }
+            set { SetProperty(ref loading, value); }
         }
 
         public void Increment()
@@ -27,6 +45,14 @@ namespace hoge.Models
         {
             await Task.Delay(3000);
             Count++;
+        }
+
+        public async Task GetProbrems()
+        {
+            Loading = true;
+            var probrems = await this.ApiGetService.ProblemAll();
+            Probrems = probrems;
+            Loading = false;
         }
     }
 }
