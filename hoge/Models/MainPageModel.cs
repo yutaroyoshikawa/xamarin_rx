2c1bbbfd72f749d60742fa59198395e29bd16d85
yutaroyoshikawa <yoshikawa.yutaro@gmail.com>
2021-07-25 19:02:26 +0900

    add: model

---
+++ hoge/Models/MainPageModel.cs
@@ -0,0 +1,32 @@
+﻿using Prism.Mvvm;
+using System.Threading.Tasks;
+
+namespace hoge.Models
+{
+    public class MainPageModel : BindableBase
+    {
+        public MainPageModel()
+        {
+            this.count = 0;
+        }
+
+        private int count;
+
+        public int Count
+        {
+            get { return count; }
+            set { SetProperty(ref this.count, value); }
+        }
+
+        public void Increment()
+        {
+            Count++;
+        }
+
+        public async Task AsyncIncrement()
+        {
+            await Task.Delay(3000);
+            Count++;
+        }
+    }
+}
---
+++ hoge/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,23 @@
+﻿using hoge.Models;
+using Prism.Mvvm;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using System;
+
+namespace hoge.ViewModels
+{
+    public class MainPageViewModel : BindableBase
+    {
+        public ReactiveProperty<int> Count { get; }
+
+        public ReactiveCommand IncrementCommand { get; } = new ReactiveCommand();
+        public AsyncReactiveCommand AsyncIncrementCommand { get; } = new AsyncReactiveCommand();
+
+        public MainPageViewModel(MainPageModel mainPageModel)
+        {
+            this.Count = mainPageModel.ObserveProperty(x => x.Count).ToReactiveProperty<int>();
+            this.IncrementCommand.Subscribe(_ => mainPageModel.Increment());
+            this.AsyncIncrementCommand.WithSubscribe(mainPageModel.AsyncIncrement);
+        }
+    }
+}
